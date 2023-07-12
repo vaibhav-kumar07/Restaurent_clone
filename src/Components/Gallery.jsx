3fc7773c166be1317d12c6167704a7e460fa2540
vaibhavkumar-07-blast <viranskh336907@gmail.com>
2023-07-12 12:33:14 +0530

    third commit

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -3,12 +3,12 @@ import image2 from "../images/image2.png"
 import image3 from "../images/image3.png"
 import image4 from "../images/image4.png"
 import image5 from "../images/image5.png"
-import InstaImages from './instaImages'
+import InstaImages from './InstaImages'
 
 
 const galleryimages=[image2,image3,image4,image5]
 export default function Gallery() {
-    console.log(galleryimages);
+   
   return (
     <section  className='gallery'>
     <div className='gallerytexts'>
